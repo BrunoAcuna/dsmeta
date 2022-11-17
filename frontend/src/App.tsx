c5e6e4bb894b4ee3b903a1292346a8b33f46c6f6
Bruno Acuna <bruno2016acuna@gmail.com>
2022-11-17 12:25:30 -0300

    Other Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
+import SalesCard from "./components/SalesCard"
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+
 
 function App() {
   return (
     <>
-      <h1>Olá mundo</h1>
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>Acuna Academy</h1>
+                <p>
+                    Desenvolvido por 
+                    <a href="https://www.instagram.com/devsuperior.ig"> @brunoacn_</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
